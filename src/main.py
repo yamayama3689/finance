08fcb14310cab0c4054c701563374109ef437b94
yamashita24343@outlook.jp <yamashita24343@outlook.jp>
2025-08-26 11:24:53 +0900

    フォルダの構成修正

---
+++ src/main.py
@@ -0,0 +1,10 @@
+import google.cloud.bigquery as bigquery
+import yfinance as yf
+import pandas as pd
+import pandas_gbq
+import functions_framework
+
+
+@functions_framework.http
+def fetch_stock_financials(request):
+    print('HelloWorld')
